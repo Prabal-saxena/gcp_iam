86958f0c5585d2c192dc63968d96c53d60cac4ae
Prabal-saxena <prabalsaxena98@gmail.com>
2025-03-28 21:21:28 -0400

    ci-cd artifactory iam role binding

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ resource "google_project_iam_member" "cloud_sql_access" {
   member  = "serviceAccount:gke-service-sa@spheric-base-448422-q9.iam.gserviceaccount.com"
 }
 
+resource "google_project_iam_member" "cicd_artifactory_access" {
+  project = "spheric-base-448422-q9"
+  role    = [
+    "roles/artifactregistry.reader",
+    "roles/storage.objectViewer"
+  ]
+  member  = "serviceAccount:ci-cd-sa@spheric-base-448422-q9.iam.gserviceaccount.com"
+}
+
 terraform {
   backend "gcs" {
     bucket  = "onlineliquorservices_bucket"
