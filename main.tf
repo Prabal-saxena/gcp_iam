6d0cfa462879bf86b4c8208e69a190f874841f24
Prabal-saxena <prabalsaxena98@gmail.com>
2026-02-21 23:33:34 -0500

    WIF implementation

--- main.tf
+++ main.tf
@@ -1,151 +1,151 @@
 resource "google_project_iam_member" "cicd_artifactory_access" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/artifactregistry.reader"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_artifactory_objectViewer" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_editor" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/editor"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_artifactory_writer" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/artifactregistry.writer"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_cloud_storage_admin" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/storage.objectAdmin"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_gke_developer" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/container.developer"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_workload_identity_user" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/iam.workloadIdentityUser"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_container_admin" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/container.admin"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_token_creator" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/iam.serviceAccountTokenCreator"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@nproject-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_cloudsql_admin" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/cloudsql.admin"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cicd_networkUser" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/compute.networkUser"
-  member  = "serviceAccount:ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "sql_proxy_cloudsql_client" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/cloudsql.client"
-  member  = "serviceAccount:sql-proxy-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:sql-proxy-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 # Secret Manager Access for all services
 resource "google_project_iam_member" "secretmanager-access-product" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:product-service-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:product-service-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "secretmanager-access-order" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:order-service-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:order-service-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "secretmanager-access-user" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:user-service-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:user-service-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 # Cloud SQL Access for Order Service
 resource "google_project_iam_member" "order-sql-client" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/cloudsql.client"
-  member  = "serviceAccount:order-service-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:order-service-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 # Cloud SQL Access for User Service
 resource "google_project_iam_member" "user-sql-client" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/cloudsql.client"
-  member  = "serviceAccount:user-service-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:user-service-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 # HELM SA IAM roles
 resource "google_project_iam_member" "ecom-helm-cluster-viewer" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/container.clusterViewer"
-  member  = "serviceAccount:ecom-helm-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ecom-helm-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ecom-helm-k8s-developer" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/container.developer"
-  member  = "serviceAccount:ecom-helm-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ecom-helm-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ecom-helm-artifact-registry" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/artifactregistry.reader"
-  member  = "serviceAccount:ecom-helm-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ecom-helm-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ecom-helm-sa-user" {
-  project = "noble-linker-471623-s6"
+  project = var.project
   role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:ecom-helm-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  member  = "serviceAccount:ecom-helm-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
 }
 
 #====================================================
 # KSA binds to GSA for WIF through IAM policy binding
 #====================================================
 resource "google_service_account_iam_member" "ksa_gsa_workload_identity_user" {
-  service_account_id = "projects/noble-linker-471623-s6/serviceAccounts/ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  service_account_id = "projects/project-3fd94e98-1d48-43cd-8c1/serviceAccounts/ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
   role = "roles/iam.workloadIdentityUser"
-  member = "serviceAccount:noble-linker-471623-s6.svc.id.goog[app-microservices/product-service-ci-cd-sa]"
+  member = "serviceAccount:project-3fd94e98-1d48-43cd-8c1.svc.id.goog[app-microservices/product-service-ci-cd-sa]"
 }
 
 resource "google_service_account_iam_member" "ksa_gsa_workload_identity_user_order_service" {
-  service_account_id = "projects/noble-linker-471623-s6/serviceAccounts/ci-cd-sa@noble-linker-471623-s6.iam.gserviceaccount.com"
+  service_account_id = "projects/project-3fd94e98-1d48-43cd-8c1/serviceAccounts/ci-cd-sa@project-3fd94e98-1d48-43cd-8c1.iam.gserviceaccount.com"
   role = "roles/iam.workloadIdentityUser"
-  member = "serviceAccount:noble-linker-471623-s6.svc.id.goog[app-microservices/order-service-ci-cd-sa]"
+  member = "serviceAccount:project-3fd94e98-1d48-43cd-8c1.svc.id.goog[app-microservices/order-service-ci-cd-sa]"
 }
 
 terraform {
   backend "gcs" {
-    bucket  = "onlineliquorservices_bucket"
+    bucket  = "onlineliquorservicesbucket"
     prefix  = "terraform/iam/tfstate"
   }
 }
